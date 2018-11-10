6e45e49ad0507526be582672f8b7fc42112fa5f1
ganzorigbattur <31978292+ganzorigbattur@users.noreply.github.com>
2018-11-10 03:49:27 -0600

    working version

--- client.c
+++ client.c
@@ -18,39 +18,45 @@ int main(int argc, char * argv[]) {
 
 	// You will need to get user name as a parameter, argv[1].
 
-	if (connect_to_server("ourserver", argv[1], pipe_user_reading_from_server, pipe_user_writing_to_server) == -1) {
+	if (connect_to_server("YOUR_UNIQUE_ID", argv[1], pipe_user_reading_from_server, pipe_user_writing_to_server) == -1) {
 		exit(-1);
 	}
 
 	/* -------------- YOUR CODE STARTS HERE -----------------------------------*/
-
+	char buffer[MAX_MSG];
+	int count;
+	int flags2 = fcntl(pipe_user_reading_from_server[0], F_SETFL, fcntl(0,F_GETFL) | O_NONBLOCK);
+	int flags = fcntl(0, F_SETFL, fcntl(0,F_GETFL) | O_NONBLOCK);
 
 	// poll pipe retrieved and print it to sdiout
 
 	// Poll stdin (input from the terminal) and send it to server (child process) via pipe
+	memset(buffer, '\0', MAX_MSG);
 	while(1){
-	int flags = fcntl(0, F_SETFL, fcntl(0,F_GETFL) | O_NONBLOCK);
-	int count;
-	char buffer[MAX_MSG];
-	count = read(0, buffer, MAX_MSG);
 
-	if (count < 0 && errno == EAGAIN){
-		// no data available
-		// set errno to EAGAIN
-	} else if(count >= 0 ){
-		// there is data
-		int a = write(pipe_user_writing_to_server[1], buffer, count);
-		printf(" what I read is %s, I read %d bytes, I wrote %d bytes\n", buffer, count, a);
+		//************ USER read from Server**************/
+		count = read(pipe_user_reading_from_server[0], buffer, MAX_MSG);
+	//	printf("reading count %d\n", count);
+		if(count > 0 ){
+			printf("%s\n", buffer);
+			memset(buffer, '\0', MAX_MSG);
+		}else if(count == 0){
+			close(pipe_user_reading_from_server[0]);
+			close(pipe_user_writing_to_server[1]);
+			printf("kicked\n" );
+			exit(-1);
+		}
 
-	} else {
-		// some error occurred during reading
- 	}
 
-	char buffer2[MAX_MSG];
-	int otherbytes = read(pipe_user_reading_from_server[0], buffer2, MAX_MSG);
-	if (otherbytes > 0) {
-		printf("%s\n", buffer2);
-	}
+		//************ USER sending to Server**************/
+		count = read(0, buffer, MAX_MSG);
+		if(count > 0 ){
+			buffer[strlen(buffer)-1] = '\0';
+			int a = write(pipe_user_writing_to_server[1], buffer, count-1);
+			memset(buffer, '\0', MAX_MSG);
+		}
+
+	usleep(500);
 }
 return 0;
 	/* -------------- YOUR CODE ENDS HERE -----------------------------------*/
